f823ff22319912665b6bbc4e5f78eb37b211c372
sannat17 <sannat1701@gmail.com>
2021-05-23 15:13:18 -0400

    Minor QOL changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer';
 
 export default function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
-  const update_time = () => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }
-
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-        <button onClick={update_time}>Update Time</button>
+        </a> */}
+        <Timer />
       </header>
     </div>
   );
