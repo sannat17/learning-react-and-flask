2d9c3748c3e0982bbc72dacbe2cd3b82c01b7176
sannat17 <sannat1701@gmail.com>
2021-05-22 23:44:02 -0400

    Learnt and implemented a state hook: Update time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  const update_time = () => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +32,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
+        <button onClick={update_time}>Update Time</button>
       </header>
     </div>
   );
 }
-
-export default App;
