6f09cd1be9de28e63d922adcd9cb2ae03cc78d95
sannat17 <sannat1701@gmail.com>
2021-05-24 14:22:44 -0400

    Added setInterval to limit the rate at which the API to fetch time is called

--- src/Timer.js
+++ src/Timer.js
@@ -3,10 +3,19 @@ import React, { useState, useEffect } from 'react';
 export default function Timer() {
     var [currDate, setCurrDate] = useState(new Date(0));
 
-    useEffect(() => {
+    const update_date_from_api = () => {
         fetch('/time').then(res => res.json()).then(data => {
-          setCurrDate(new Date(data.time * 1000));
+            setCurrDate(new Date(data.time * 1000));
         });
+    }
+
+    useEffect(() => {
+        var timer = setInterval(update_date_from_api, 1000);
+
+        return function cleanup() {
+            clearInterval(timer);
+        }
+
     }, []);
     
     return (
